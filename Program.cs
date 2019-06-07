eec777d04b844440d093413cf810c6238e40381d
JMONDESIR <Joey.M.Dev@gmail.com>
2019-06-07 14:41:35 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Random_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+};
+            for (int i = 0; i < numbers.Count; i++)
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine("Number list contains " + i);
+                }
+                else
+                {
+                    Console.WriteLine("Number list does not contain " + i);
+                }
+        }
+    }
+}
+
